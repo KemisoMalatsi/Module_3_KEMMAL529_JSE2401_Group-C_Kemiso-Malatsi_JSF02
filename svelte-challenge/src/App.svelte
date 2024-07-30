d04c24481959cb617bd59189f01740da57c92710
KemisoMalatsi <kemiso.malatsi@gmail.com>
2024-07-30 11:11:11 +0200

    Installed tailwind packages and its dependencies from LogRocket

---
+++ svelte-challenge/src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+  import TailwindCss from './lib/TailwindCSS.svelte';
+</script>
+
+<TailwindCss/>
+
+<main>
+  <h1 class="py-8 px-4 border border-indigo-900 shadow-lg">Hello {name}!</h1>
+  <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
